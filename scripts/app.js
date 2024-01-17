103e33d8833faafacffa52d0e5df0ca89f0cc4fd
JayvonK <jaykhuth@gmail.com>
2024-01-17 09:00:04 -0800

    save

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ let playBtn = document.getElementById("playBtn");
 let modeBtn = document.getElementById("modeBtn");
 let howToPlayBtn = document.getElementById("howToPlayBtn");
 let music = document.getElementById("music");
+let volume = document.getElementById("volume");
 
 playBtn.addEventListener('click', (event) => {
     modeBtn.src = "";
