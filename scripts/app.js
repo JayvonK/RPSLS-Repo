572d8452cb6bbf8ea912c60daca6bf37fe0cf091
JayvonK <jaykhuth@gmail.com>
2024-01-17 09:09:21 -0800

    music

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,31 @@ let howToPlayBtn = document.getElementById("howToPlayBtn");
 let music = document.getElementById("music");
 let volume = document.getElementById("volume");
 
+function musicPlay(){
+    music.play();
+}
+
+musicPlay();
+
+function stopMuic() {
+    music.pause();
+}
+
+
+volume.addEventListener('click', () => {
+    if(volume.src === "./assets/volume icon.png")
+    {
+        musicPlay();
+        volume.src = "./assets/mute icon.png";
+        console.log("i woke");
+    } else if (volume.src === "./assets/mute icon.png")
+    {
+        stopMuic();
+        volume.src = "./assets/volume icon.png";
+        console.log("i wokde");
+    }
+})
+
 playBtn.addEventListener('click', (event) => {
     modeBtn.src = "";
     howToPlayBtn.src = "";
