535c75fbf7c1037322ffd5cca3f41ea431595008
JayvonK <jaykhuth@gmail.com>
2024-01-18 19:25:34 -0800

    creating elements

--- scripts/addFights.js
+++ scripts/addFights.js
@@ -4,7 +4,7 @@ let fightDiv = document.getElementById("fightDiv");
 let randomBool = false;
 
 
-function addSuddenDeathFight() {
+function addSuddenDeathFight(point1, point2, message1, message2) {
     let p1ChoicesOuterDiv = document.createElement("div");
     p1ChoicesOuterDiv.className = "center"
 
@@ -56,6 +56,124 @@ function addSuddenDeathFight() {
     let p1Col = document.createElement("div");
     p1Col.className = "col samurai1Padding";
 
+    let p1Score = document.createElement("h1");
+    p1Score.className = "imPerfect white txtCenter score";
+    p1Score.innerText = `${point1}/1`;
+    
+    let samurai1ImgDiv = document.createElement("div");
+    samurai1ImgDiv.className = "center";
+
+    let samurai1Img = document.createElement("img");
+    samurai1Img.src = "./assets/samurai 1.png";
+    samurai1Img.className = "samuraiSize";
+    samurai1Img.alt = "picture of samurai";
+
+    samurai1ImgDiv.appendChild(samurai1Img);
+
+    let samurai1Txt = document.createElement("p");
+    samurai1Txt.className = "imPerfect txtCenter cyan samurai";
+    samurai1Txt.innerText = "Samurai 1";
+
+    let choose1 = document.createElement("h1");
+    choose1.className = "imPerfect txtCenter cyan choose";
+    choose1.innerText = "choose";
+
+    let dontCheat1 = document.createElement("p");
+    dontCheat1.innerText = message1;
+    dontCheat1.className = "imPerfect txtCenter cyan message";
+
+    p1Col.appendChild(p1Score);
+    p1Col.appendChild(samurai1ImgDiv);
+    p1Col.appendChild(samurai1Txt);
+    p1Col.appendChild(choose1);
+    p1Col.appendChild(p1ChoicesOuterDiv);
+    p1Col.appendChild(dontCheat1);
+
+
+
+    let p2ChoicesOuterDiv = document.createElement("div");
+    p2ChoicesOuterDiv.className = "center"
+
+    let p2choices = document.createElement("div");
+    p2choices.className = "between optionsDiv";
+
+    let rockImg2 = document.createElement("img");
+    rockImg2.src = "./assets/rock icon.png";
+    rockImg2.alt = "img of rock";
+    rockImg2.addEventListener('click', (event) => {
+        p2Choice = "rock";
+    })
+
+    let paperImg2 = document.createElement("img");
+    paperImg2.src = "./assets/paper icon.png";
+    paperImg2.alt = "img of paper";
+    paperImg2.addEventListener('click', (event) => {
+        p2Choice = "paper";
+    })
+
+    let scissorsImg2 = document.createElement("img");
+    scissorsImg2.src = "./assets/scissors icon.png";
+    scissorsImg2.alt = "img of scissors";
+    scissorsImg2.addEventListener('click', (event) => {
+        p2Choice = "scissors";
+    })
+
+    let lizzardImg2 = document.createElement("img");
+    lizzardImg2.src = "./assets/lizard icon.png";
+    lizzardImg2.alt = "img of lizzard";
+    lizzardImg2.addEventListener('click', (event) => {
+        p2Choice = "lizzard";
+    })
+
+    let spockImg2 = document.createElement("img");
+    spockImg2.src = "./assets/spock icon.png";
+    spockImg2.alt = "img of spock";
+    spockImg2.addEventListener('click', (event) => {
+        p2Choice = "spock";
+    })
+
+    p2choices.appendChild(rockImg2);
+    p2choices.appendChild(paperImg2);
+    p2choices.appendChild(scissorsImg2);
+    p2choices.appendChild(lizzardImg2);
+    p2choices.appendChild(spockImg2);
+    p2ChoicesOuterDiv.appendChild(p2choices);
+
+    let p2Col = document.createElement("div");
+    p2Col.className = "col samurai2Padding";
+
+    let p2Score = document.createElement("h1");
+    p2Score.className = "imPerfect white txtCenter score";
+    p2Score.innerText = `${point2}/1`;
+    
+    let samurai2ImgDiv = document.createElement("div");
+    samurai2ImgDiv.className = "center";
+
+    let samurai2Img = document.createElement("img");
+    samurai2Img.src = "./assets/samurai 2.png";
+    samurai2Img.className = "samuraiSize";
+    samurai2Img.alt = "picture of samurai";
+
+    samurai2ImgDiv.appendChild(samurai2Img);
+
+    let samurai2Txt = document.createElement("p");
+    samurai2Txt.className = "imPerfect txtCenter yellow samurai";
+    samurai2Txt.innerText = "Samurai 2";
+
+    let choose2 = document.createElement("h1");
+    choose2.className = "imPerfect txtCenter yellow choose";
+    choose2.innerText = "choose";
+
+    let dontCheat2 = document.createElement("p");
+    dontCheat2.innerText = message1;
+    dontCheat2.className = "imPerfect txtCenter yellow message";
+
+    p2Col.appendChild(p2Score);
+    p2Col.appendChild(samurai2ImgDiv);
+    p2Col.appendChild(samurai2Txt);
+    p2Col.appendChild(choose2);
+    p2Col.appendChild(p2ChoicesOuterDiv);
+    p2Col.appendChild(dontCheat2);
 
 
 
